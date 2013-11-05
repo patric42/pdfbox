0d46f8d5ea3dd58c85780d4442d67a17fbe9fc33
Patric Bechtel <p.bechtel@oashi.com>
2013-11-06 00:27:16 +0100

    set renderinghints in sync with system and highest quality.

--- pdfbox/src/main/java/org/apache/pdfbox/pdfviewer/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfviewer/PageDrawer.java
@@ -116,8 +116,20 @@ public class PageDrawer extends PDFStreamEngine
         graphics.setStroke(new BasicStroke(10f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER));
         page = p;
         pageSize = pageDimension;
+
+        Map dh = (Map)Toolkit.getDefaultToolkit().getDesktopProperty("awt.font.desktophints");
+        if (dh!=null) {
+           graphics.setRenderingHints(dh);
+        }
+
         graphics.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+        graphics.setRenderingHint( RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY );
+        graphics.setRenderingHint( RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE );
         graphics.setRenderingHint( RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON );
+        graphics.setRenderingHint( RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC );
+        graphics.setRenderingHint( RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY );
+        graphics.setRenderingHint( RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY );
+        graphics.setRenderingHint( RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE );
         // Only if there is some content, we have to process it. 
         // Otherwise we are done here and we will produce an empty page
         if ( page.getContents() != null) 
